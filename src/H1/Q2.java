2303c3ea1a527b980c5a431feb7fe21f7b5d48b0
Gokhan Korkmaz <gokankorkmaz@gmail.com>
2022-11-21 13:03:34 +0300

    Q2 added

---
+++ src/H1/Q2.java
@@ -0,0 +1,15 @@
+package H1;
+
+public class Q2 {
+}
+
+
+
+
+/*
+Given an array of ints length 3, return the sum of all the element
+
+        sum3([1, 2, 3]) → 6
+        sum3([5, 11, 2]) → 18
+        sum3([7, 0, 0]) → 7
+ */
