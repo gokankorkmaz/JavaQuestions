3eafa6c931f28bc443855c6b06c0c6857ce3ac81
Gokhan Korkmaz <gokankorkmaz@gmail.com>
2022-11-22 21:11:14 +0300

    Q3 completed

---
+++ src/H1/Q3.java
@@ -0,0 +1,18 @@
+package H1;
+
+public class Q3 {
+
+    public static void main(String[] args) {
+        for (int i = 100; i >0 ; i--) {
+            if(i%13==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+
+/*
+Print the numbers from 100 to 0 which are divisible by 13 (from largest to smallest).
+ */
